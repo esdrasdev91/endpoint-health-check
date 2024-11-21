69b1d73fba37cc47a15acbfc125c0941aa66127b
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-11-21 16:36:22 -0300

    ajusts class controller

--- src/main/java/com/esdrasdev/endpoint_health_check/controller/HealthCheckController.java
+++ src/main/java/com/esdrasdev/endpoint_health_check/controller/HealthCheckController.java
@@ -1,18 +1,16 @@
 package com.esdrasdev.endpoint_health_check.controller;
 
 import com.esdrasdev.endpoint_health_check.service.CustomHealthIndicator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HealthCheckController {
 
     private final CustomHealthIndicator customHealthIndicator;
-
-    public HealthCheckController(CustomHealthIndicator customHealthIndicator) {
-        this.customHealthIndicator = customHealthIndicator;
-    }
 
     @GetMapping("/custom-health")
     public Health customHealth() {
